cd4eef19d70bade451ea094cbf186eb9fde4f934
Aritrajit <aritrajit2000sarkar@gmail.com>
2022-01-06 13:02:52 +0530

    Add files via upload

---
+++ eye-candy.py
@@ -0,0 +1,9 @@
+import turtle
+
+t = turtle.Turtle()
+list1 = ["purple", "red", "green", "orange", "blue"]
+for i in range(200):
+  t.color(list1[i % 5])
+  t.pensize(i/10+1)
+  t.forward(i)
+  t.left(59)
